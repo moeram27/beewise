14a8648ed897538aad7b29e089498df2bc74502f
moeram27 <mohamadr2003@gmail.com>
2025-02-18 12:30:34 +0200

    first commit

---
+++ BeeWise/Controllers/BeeWiseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeeWise.Controllers
+{
+    public class BeeWiseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
