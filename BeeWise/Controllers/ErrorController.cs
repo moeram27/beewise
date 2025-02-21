2beac1a85c1c8dfda21a56f45f68e8c4f6622921
moeram27 <mohamadr2003@gmail.com>
2025-02-21 12:16:17 +0200

    IDRK WHATS GOING ON

---
+++ BeeWise/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeeWise.Controllers
+{
+    [Route("[controller]")]
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Error");
+        }
+            //return View("~/Views/Shared/Error.cshtml");
+
+    }
+}
