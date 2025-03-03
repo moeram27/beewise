736a4760171c11d1c616fda6dd5eb89916385948
moeram27 <mohamadr2003@gmail.com>
2025-03-03 15:16:30 +0200

    design implementations

--- BeeWise/obj/Debug/net8.0/BeeWise.AssemblyInfo.cs
+++ BeeWise/obj/Debug/net8.0/BeeWise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeeWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+647bcb015c8cc5dcc8678bdaa0e92c3df2b76a1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36dc5ed41611a0987bc092eb5d45d0961fd94c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeeWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeeWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
